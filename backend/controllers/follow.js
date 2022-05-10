5c7644c51f60c8f2f1c47fcc5c0605a2dc321d47
AhmadTayseer <ahmad.ty.taha@gmail.com>
2022-05-10 22:52:31 +0300

    Added Follow function in (follow controller) that will add the user for following array , and added the other user to follower array

--- backend/controllers/follow.js
+++ backend/controllers/follow.js
@@ -41,49 +41,43 @@ const userModel = require("../models/usersShema");
 //   //     res.json({ err: err });
 //   //   }
 // };
-const follow = (req, res) => {
-    const _id = req.params.id;
-  
-    //   const followId = req.token.userId;
-    console.log("====");
-    console.log(_id);
-  
-    //   console.log(followId);
-    userModel
-      .updateOne(
-        { _id },
+const follow = async (req, res) => {
+  const params = req.params.id;
+
+  const signInUser = req.token.userId;
+
+  try {
+    const addToFollower = await userModel.updateOne(
+      { _id: params },
+      {
+        $push: { followers: signInUser },
+      },
+      { new: true }
+    );
+
+    if (addToFollower) {
+      const addToFollowing = await userModel.updateOne(
+        { _id: signInUser },
         {
-          $push: { following: "627953ff328ea38308dfebf5" },
+          $push: { following: params },
         },
         { new: true }
-      )
-      .then((result) => {
-        res.json({ success: result });
-      })
-      .catch((err) => {
-        console.log(err);
-        res.json({ err: err });
+      );
+      console.log(addToFollower, "Add To Follower");
+      console.log(addToFollowing, "Add To Following");
+
+      return res.status(201).json({
+        success: true,
+        message: "Added successfully",
       });
-  
-    // if (addToFollower) {
-    //   const addToFollowing = await userModel.findOneAndUpdate(
-    //     { _id: followId },
-    //     {
-    //       $push: { following: params },
-    //     },
-    //     { new: true }
-    //   );
-    // res.json({ success: true });
-    // }
-  
-    //   console.log(addToFollowing , "=====");
-    //     console.log(addToFollower, "====");
-    //   } catch (err) {
-    //     res.json({ err: err });
-    //   }
-  };
-
-
+    }
+  } catch (err) {
+    res.status(500).json({
+      messge: "Server Error",
+      err: err.message,
+    });
+  }
+};
 
 //the unfollow will be the same with pull operator instead of push ....
 
