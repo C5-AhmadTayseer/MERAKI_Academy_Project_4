4fcd63a3850193acc7d61ac561f1b35334e02892
AhmadTayseer <ahmad.ty.taha@gmail.com>
2022-05-11 20:25:02 +0300

    prepare FE files to start working on it ,  installed router-dom and axios , wrapped BrowserRouter around app component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
+import React from "react";
+import "./App.css";
 
-import './App.css';
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1>Hello world</h1> 
+{/* side Bar , to be fixed before routes  */}
+      <Routes></Routes>
     </div>
   );
 }
