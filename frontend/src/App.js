dc5be9304bb139b7bc3ac7131dc9c35dee19143f
AhmadTayseer <ahmad.ty.taha@gmail.com>
2022-05-11 21:17:53 +0300

    finished register component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
 import "./App.css";
+import Register from "./components/Register";
 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1> 
-{/* side Bar , to be fixed before routes  */}
-      <Routes></Routes>
+      <h1>Hello world</h1>
+      {/* Links just for testeing */}
+
+      <Link to="/register">Register</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/home">home</Link>
+      {/* side Bar , to be fixed before routes?  */}
+
+      <Routes>
+        <Route path="/register" element={<Register />}></Route>
+      </Routes>
     </div>
   );
 }
--- frontend/src/components/Register/index.js
+++ frontend/src/components/Register/index.js
@@ -1,7 +1,49 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
 
-export const Register = () => {
+const Register = () => {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const addUser = () => {
+    const newUser = { userName, password, email };
+    console.log(newUser);
+    axios
+      .post("http://localhost:5000/register", newUser)
+      .then((result) => {
+        console.log(result, "Regiter Result");
+      })
+      .catch((err) => {
+        console.log(err, "Register Error");
+      });
+  };
+
   return (
-    <div>Register</div>
-  )
-}
+    <div>
+      <div className="inputs">
+        <input
+          placeholder="userName"
+          onChange={(e) => {
+            setUserName(e.target.value);
+          }}
+        />
+        <input
+          placeholder="email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <input
+          placeholder="password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <button onClick={addUser}>Register</button>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
